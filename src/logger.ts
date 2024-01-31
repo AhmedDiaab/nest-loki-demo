59a64e01d2140d925398399a52147af5e34c559f
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-01-31 13:18:03 +0200

    feat(): add fallback to files

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,12 @@
 import { LoggerService, ConsoleLogger, LogLevel } from "@nestjs/common";
 import * as axios from 'axios';
+import { createLogger, Logger } from 'winston';
+import * as DRTransport from 'winston-daily-rotate-file';
 
 export type LogOptions = {
     console: boolean;
     loki: boolean;
+    fallbackToFile: boolean;
 }
 
 export type LokiLog = {
@@ -22,15 +25,35 @@ type LokiStreamInfo = {
 
 export class LokiLogger extends ConsoleLogger implements LoggerService {
 
-    private readonly _options: LogOptions = { console: true, loki: false };
+    private readonly _options: LogOptions = { console: true, loki: false, fallbackToFile: true };
     private readonly instance: axios.AxiosInstance;
     private readonly serviceName: string;
+    private readonly logger: Logger;
 
 
     constructor(context: string, options?: Partial<LogOptions>) {
         super(context);
         this.serviceName = context;
         if (options) this._options = { ...this._options, ...options };
+
+        // create logger
+        this.logger = createLogger();
+
+        if (this._options.fallbackToFile) {
+
+            // create rotating file if loki is down
+            const fileTransport = new DRTransport({
+                eol: '\n',
+                datePattern: 'YYYY-MM-DD',
+                utc: true,
+                createSymlink: true, // for creating link for current log file
+                symlinkName: 'current.log',
+                dirname: `./logs/`,
+                filename: 'log-%DATE%.log',
+            });
+
+            this.logger.add(fileTransport);
+        }
         this.instance = axios.default.create({
             baseURL: "http://localhost:3100",
             headers: {
@@ -41,28 +64,22 @@ export class LokiLogger extends ConsoleLogger implements LoggerService {
 
     async log(message: string) {
         this._options.console && super.log(message);
-        this._options.loki && await this._logToLoki(message, 'log');
+        await this._fallbackToFile(message, 'log');
     }
 
     async error(message: string, trace: string) {
         this._options.console && super.error(message, trace);
-        this._options.loki && await this._logToLoki(message, 'error');
+        await this._fallbackToFile(message, 'error');
     }
 
     async warn(message: string) {
         this._options.console && super.warn(message);
-        this._options.loki && await this._logToLoki(message, 'error');
-
+        await this._fallbackToFile(message, 'warn');
     }
 
     async debug(message: string) {
         this._options.console && super.debug(message);
-        this._options.loki && await this._logToLoki(message, 'error');
-    }
-
-    async verbose(message: string) {
-        this._options.console && super.verbose(message);
-        this._options.loki && await this._logToLoki(message, 'error');
+        await this._fallbackToFile(message, 'debug');
     }
 
     private async _logToLoki(message: string, level: LogLevel) {
@@ -72,18 +89,20 @@ export class LokiLogger extends ConsoleLogger implements LoggerService {
                 values: [[(Date.now() * 1e6).toString(), message]]
             }]
         };
-
-        const _log = {
-            stream: { env: 'development', service: this.serviceName },
-            values: [(Date.now() * 1e6).toString(), message]
-        };
         try {
             await this.instance.post('/loki/api/v1/push', log)
         }
         catch (e) {
             console.error('Could not log to loki | ', `Level=${level}`);
-            console.log(e)
+            throw e;
         }
+    }
 
+    private async _fallbackToFile(message: string, level: LogLevel) {
+        try {
+            this._options.loki && await this._logToLoki(message, 'error');
+        } catch (_) {
+            this.logger.log('log', message);
+        }
     }
 }
