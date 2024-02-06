d0f3b8377ce684413031c66eaa6c638dc1df28a6
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-02-06 12:49:06 +0200

    feat(): import modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,43 @@ import { AppService } from './app.service';
 import { LoggingInterceptor } from './logging.interceptor';
 import { HttpModule } from '@nestjs/axios';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { BullModule } from '@nestjs/bull';
+import { BullBoardModule } from "@bull-board/nestjs";
+import { ExpressAdapter } from "@bull-board/express";
+import { EVENT_EMITTER_TOKEN } from './event-emitter.token';
+import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
-    HttpModule
+    HttpModule,
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      }
+    }),
+    BullModule.registerQueue({
+      name: 'logs-queue',
+    }),
+    BullBoardModule.forRoot({
+      route: '/queues',
+      adapter: ExpressAdapter // Or FastifyAdapter from `@bull-board/fastify`
+    }),
+    EventEmitterModule.forRoot({
+      global: true,
+      
+    })
   ],
   controllers: [AppController],
-  providers: [AppService, {
-    provide: APP_INTERCEPTOR,
-    useClass: LoggingInterceptor,
-  }],
+  providers: [
+    AppService, {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+    {
+      provide: EVENT_EMITTER_TOKEN,
+      useValue: new EventEmitter2()
+    }
+  ],
 })
 export class AppModule { }
