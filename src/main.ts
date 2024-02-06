8e92327455c6d1f449263ccfd05c5db15405d44a
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-02-06 12:56:51 +0200

    feat(): use filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExceptionsFilter } from './exceptions/exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
+  app.useGlobalFilters(new ExceptionsFilter())
   await app.listen(3020);
 }
 bootstrap();
