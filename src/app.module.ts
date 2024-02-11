a5c71855efe3c2bfa8d8235bae2acac3ae08295f
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-02-11 14:29:52 +0200

    feat(): remove exception filter and register it in app module, add bull and bull board imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingInterceptor } from './logging.interceptor';
 import { HttpModule } from '@nestjs/axios';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { BullModule } from '@nestjs/bull';
 import { BullBoardModule } from "@bull-board/nestjs";
 import { ExpressAdapter } from "@bull-board/express";
-import { EVENT_EMITTER_TOKEN } from './event-emitter.token';
-import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { LogsListener } from './logs.listener';
+import { join } from 'path';
+import { LokiLogger } from './logger';
+import { ExceptionsFilter } from './exceptions/exceptions.filter';
+import { BullAdapter } from '@bull-board/api/bullAdapter'
+import { LOGS_QUEUE } from './logs.token';
+import { LogsProcessor } from './logs.processor';
 
 @Module({
   imports: [
@@ -20,27 +26,36 @@ import { EventEmitter2, EventEmitterModule } from '@nestjs/event-emitter';
       }
     }),
     BullModule.registerQueue({
-      name: 'logs-queue',
+      name: LOGS_QUEUE
     }),
     BullBoardModule.forRoot({
       route: '/queues',
       adapter: ExpressAdapter // Or FastifyAdapter from `@bull-board/fastify`
     }),
+    BullBoardModule.forFeature({
+      name: LOGS_QUEUE,
+      adapter: BullAdapter
+    }),
     EventEmitterModule.forRoot({
       global: true,
-      
+      verboseMemoryLeak: true
+
     })
   ],
   controllers: [AppController],
   providers: [
-    AppService, {
+    AppService,
+    {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
     },
     {
-      provide: EVENT_EMITTER_TOKEN,
-      useValue: new EventEmitter2()
-    }
+      provide: APP_FILTER,
+      useClass: ExceptionsFilter
+    },
+    LokiLogger,
+    LogsListener,
+    LogsProcessor
   ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
-  app.useGlobalFilters(new ExceptionsFilter())
   await app.listen(3020);
 }
 bootstrap();
