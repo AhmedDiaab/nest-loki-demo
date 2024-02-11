f4a3e7ab095dd982faf03ad9af5fdf07844ec929
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-02-11 17:36:48 +0200

    feat(): add events for logs

--- src/logs.events.ts
+++ src/logs.events.ts
@@ -1,3 +1,7 @@
 import { OnEventType } from '@nestjs/event-emitter';
 
 export const ProcessLogsEvent: OnEventType = 'logs.process';
+
+export const RetryFailedLogsEvent: OnEventType = 'logs.retry';
+
+export const MarkRetrialFlag: OnEventType = 'logs.markRetrial';
