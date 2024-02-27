5e7b70fbbb41bc741e26d9863c9927502312a1d9
AhmedDiaab <ahmed_20171801@fci.helwan.edu.eg>
2024-02-27 15:27:19 +0200

    feat(): register interceptor and filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
 import { PinoConfig } from './pino.config';
 import { LoggingInterceptor } from './logging.interceptor';
 import { LokiLogger } from './logger';
+import { ExceptionsFilter } from './exceptions/exceptions.filter';
 
 @Module({
   imports: [
@@ -18,6 +19,10 @@ import { LokiLogger } from './logger';
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionsFilter
     }
   ],
 })
